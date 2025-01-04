34ea8b143aacbc5d9d4dbadd11e42532a16c31c4
Nishant Krishna Ghadigaonkar <nishantkrishna1288@gmail.com>
2025-01-04 05:50:50 +0530

    Host binding

--- src/app/highlight-element.directive.ts
+++ src/app/highlight-element.directive.ts
@@ -1,10 +1,38 @@
-import { Directive, HostBinding } from '@angular/core';
+import { Directive, HostBinding, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appHighlightElement]'
 })
 export class HighlightElementDirective {
-@HostBinding('style.backgroundColor') bgColor = '#f2aa4cff';
+
+  colorArray = ['red', 'blue', 'yellow', 'orange', 'green'];
+
+  @HostBinding('style.backgroundColor') bgColor = '#f2aa4cff';
+
+
+  @HostListener('click') onToggle() {
+    // if (this.bgColor === '#f2aa4cff') {
+    //   this.bgColor = 'red';
+    //   console.log('faf to red');
+    // }
+    // else {
+    //   this.bgColor = '#f2aa4cff';
+    //   console.log('red to faf');
+    // }
+    this.setRandomColor();
+  }
   constructor() { }
+
+  setRandomColor() {
+    let randomNum = Math.floor(Math.random() * this.colorArray.length);
+    let color = this.colorArray[randomNum];
+
+    if (this.bgColor !== color) {
+      this.bgColor = color;
+    }
+    else {
+      this.setRandomColor();
+    }
+  }
 
 }
